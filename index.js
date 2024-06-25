a4c58160314e7495b3965fb859a6575b50193b6b
Japnoor Singh <sjapnoor11@gmail.com>
2024-06-25 20:40:37 +0530

    Errors corrected

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const path = require('path');
 const { User, Quiz } = require('./models/quiz');
 const session = require('express-session');
 const https = require('https');
-const app = express();
-const port = process.env.PORT || 4000;
 const mongoose = require('mongoose');
 
 const { initializeApp } = require("firebase/app");
-const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
+const { getStorage, ref, getDownloadURL } = require("firebase/storage");
 
+const app = express();
+const port = process.env.PORT || 4000;
 
 const firebaseConfig = {
     apiKey: process.env.FB_API_KEY,
@@ -27,23 +27,18 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-
 mongoose.connect(process.env.DB_URL);
 const db = mongoose.connection;
 
 db.on('error', (error) => {
     console.log("DB error : ", error);
-})
+});
 db.once('open', () => {
     console.log("Connected to database");
-
 });
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('views'));
-// app.use(express.static('images'));
-
 
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
@@ -54,114 +49,76 @@ app.use(session({
     saveUninitialized: true
 }));
 
-
-
 app.get('/', (req, res) => {
-
     if (req.session.visitedRoutes) {
         req.session.visitedRoutes = [];
     }
-
     res.render("index");
-})
+});
 
 app.get('/exists', (req, res) => {
     res.render('index');
 });
 
 app.post('/details', (req, res) => {
-    var { name, email, phone, institution } = req.body;
-    console.log("dd", phone);
+    const { name, email, phone, institution } = req.body;
     req.session.name = name;
     req.session.email = email;
     req.session.phone = phone;
     req.session.institution = institution;
     res.redirect('/quiz');
-
 });
 
-// function checkVisited(req, res, next) {
-//     if (req.session.visitedRoutes && req.session.visitedRoutes.includes(req.originalUrl)) {
-//         return res.redirect('/');
-//     }
-
-//     if (!req.session.visitedRoutes) {
-//         req.session.visitedRoutes = [];
-//     }
-
-//     req.session.visitedRoutes.push(req.originalUrl);
-
-//     next();
-// }
-
 app.get('/quiz', async (req, res) => {
-
     try {
         const target = new Date().toDateString() + ".xlsx";
         const quiz = await Quiz.findOne({ quiz_name: target }).exec();
         if (quiz) {
             const { quiz_name } = quiz;
-            const fileName = quiz_name;
-            // const bucket = admin.storage().bucket();
-            // const file = bucket.file(fileName);
-
-
-            // const destination = './uploads/excel-file.xlsx';
-
-            // await file.download({ destination });
-
             const storage = getStorage(firebaseApp);
-
-            const fileRef = ref(storage, '/' + fileName);
+            const fileRef = ref(storage, '/' + quiz_name);
 
             getDownloadURL(fileRef)
                 .then((url) => {
                     const file = fs.createWriteStream("./uploads/excel-file.xlsx");
-
                     https.get(url, function (response) {
                         response.pipe(file);
-                        console.log('Excel file downloaded successfully.');
+                        file.on('finish', async () => {
+                            file.close();
+                            console.log('Excel file downloaded successfully.');
+                            const workbook = new Excel.Workbook();
+                            await workbook.xlsx.readFile("./uploads/excel-file.xlsx");
+                            const worksheet = workbook.getWorksheet(1);
+                            const questions = [];
+                            worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
+                                const question = row.getCell(1).value;
+                                const options = [];
+                                for (let i = 2; i <= 5; i++) {
+                                    options.push(row.getCell(i).value);
+                                }
+                                const correctAnswer = row.getCell(6).value;
+                                questions.push({ question, options, correctAnswer });
+                            });
+                            res.render('quiz', { questions });
+                        });
                     });
                 })
                 .catch((error) => {
                     console.error(`Failed to download file: ${error}`);
+                    res.status(500).send('Internal Server Error');
                 });
-            const filePath = './uploads/excel-file.xlsx';
-            const workbook = new Excel.Workbook();
-            await workbook.xlsx.readFile(filePath);
-
-            const worksheet = workbook.getWorksheet(1);
-            const questions = [];
-
-            worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
-                const question = row.getCell(1).value;
-                const options = [];
-                let correctAnswer;
-
-                for (let i = 2; i <= 5; i++) {
-                    options.push(row.getCell(i).value);
-                }
-
-                correctAnswer = row.getCell(6).value;
-
-                questions.push({ question, options, correctAnswer });
-            });
-
-            res.render('quiz', { questions: questions });
-        }
-        else {
+        } else {
             res.redirect('/quizNotFound');
         }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
-    catch (err) {
-        console.log(err);
-    }
-
-})
+});
 
 app.get('/quizNotFound', (req, res) => {
     res.sendFile(path.join(__dirname, "views", "quizNotFound.html"));
-})
+});
 
 app.post('/submit', async (req, res) => {
     const { quiz } = req.body;
@@ -169,23 +126,18 @@ app.post('/submit', async (req, res) => {
         const filePath = "./uploads/excel-file.xlsx";
         const workbook = new Excel.Workbook();
         await workbook.xlsx.readFile(filePath);
-
         const worksheet = workbook.getWorksheet(1);
         const questions = [];
-
         worksheet.eachRow({ includeEmpty: false }, function (row, rowNumber) {
             const question = row.getCell(1).value;
             const options = [];
-            let correctAnswer;
-
             for (let i = 2; i <= 5; i++) {
                 options.push(row.getCell(i).value);
             }
-
-            correctAnswer = row.getCell(6).value;
-
+            const correctAnswer = row.getCell(6).value;
             questions.push({ question, options, correctAnswer });
         });
+
         let score = 0;
         if (quiz) {
             for (let i = 0; i < questions.length; i++) {
@@ -194,22 +146,22 @@ app.post('/submit', async (req, res) => {
                 }
             }
         }
-        console.log(req.session.phone);
-        const user = new User({ name: req.session.name, email: req.session.email, phone: req.session.phone, score: score, institution: req.session.institution });
+
+        const user = new User({ name: req.session.name, email: req.session.email, phone: req.session.phone, score, institution: req.session.institution });
         const target = new Date().toDateString() + ".xlsx";
-        await Quiz.findOneAndUpdate({
-            quiz_name: target
-        }, { $push: { user: user } }, { upsert: true, new: true }
+        await Quiz.findOneAndUpdate(
+            { quiz_name: target },
+            { $push: { user } },
+            { upsert: true, new: true }
         ).exec();
+
         req.session.score = score;
         req.session.total = questions.length;
         res.redirect('/result');
-    }
-    catch (err) {
+    } catch (err) {
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
-
 });
 
 app.get('/result', (req, res) => {
