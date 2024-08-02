b4ab66a6dedf099aa9350abf0094f2e4b0c6a142
Japnoor Singh <sjapnoor11@gmail.com>
2024-08-02 20:23:08 +0530

    Deploying

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import logo from '../assets/logo.png'
 const Navbar = () => {
   return (
     <nav>
         <div className='gap-4 flex h-[70px] bg-tan-dark items-center px-4'>
-            <img src="http://www.kickdrugs.com.au/wp-content/uploads/2018/05/Kickdrugs-2d-gold.png" alt="" className='h-[60px] w-[60px]' />
+            <img src={logo} alt="" className='h-[60px] w-[60px]' />
             <span className='text-brown-dark font-medium text-2xl'>Kick Drugs Australia</span>
         </div>
     </nav>
