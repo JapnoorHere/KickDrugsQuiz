762a4420c4b9b1e3fafef07c952208cb9dcb1782
Japnoor Singh <sjapnoor11@gmail.com>
2024-08-18 23:11:03 +0530

    Changes

--- server/index.js
+++ server/index.js
@@ -38,11 +38,7 @@ db.once('open', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-app.use(cors(
-    {
-        origin: ["http://localhost:3000"]
-    }
-));
+app.use(cors());
 
 
 
