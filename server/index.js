5d77609542c2e693767677b4378374ea3243208e
Japnoor Singh <sjapnoor11@gmail.com>
2024-08-18 23:04:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,9 @@ db.once('open', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
-app.use(cors());
+app.use(cors(
+    origin : ["https://kickdrugsquizadmin.netlify.app"]
+));
 
 
 
